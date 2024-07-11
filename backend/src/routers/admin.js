63a7a1f3ae9952afdf4cf07194b5ea47fdbbd421
Serenula <serenula@gmail.com>
2024-07-11 17:18:53 +0800

    added routers

--- backend/src/routers/admin.js
+++ backend/src/routers/admin.js
@@ -10,12 +10,13 @@ const {
 const auth = require("../middleware/auth");
 const superAdminAuth = require("../middleware/superAdminAuth");
 const adminAuth = require("../middleware/adminAuth");
+const { createAdmin } = require("../controllers/admin");
 
 router.get("/users", auth, adminAuth, getAllUsers);
 router.get("/users/:id", auth, adminAuth, getUserById);
 router.put("/users/:id", auth, adminAuth, updateUserById);
 router.delete("/users/:id", auth, adminAuth, deleteUserById);
 router.put("/users/:id/role", auth, adminAuth, changeUserRole);
-router.post("/", auth, superAdminAuth);
+router.post("/", auth, superAdminAuth, createAdmin);
 
 module.exports = router;
